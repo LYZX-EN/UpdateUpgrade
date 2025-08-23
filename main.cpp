1a532cc1c9de553d887acd6c0eed70482f1e85a0
LYZX-EN <ixdgta5bxi@gmail.com>
2025-08-23 12:37:16 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main () {
+    cout << "Update - Upgrade";
+    cout << endl << "---------" << endl;
+    system("sudo apt update");
+    cout << "---------" << endl;
+    system("sudo apt upgrade -y");
+    cout << "---------" << endl;
+    system("flatpak update -y");
+    cout << "---------" << endl;
+}
